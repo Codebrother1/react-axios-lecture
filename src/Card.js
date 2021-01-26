75b43d4b7b1a5ad2b3576c7d7f10361c81054c3d
Terrance Cazy <terrancecazy@MacBook-Pro.lan>
2021-01-26 13:21:31 -0600

    lecture code along with adam

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Card = (props) => {
     return <div className="card">
-        
+        <img src={props.imageUrl} alt={props.name}/>
+        <span>{props.name}</span>
     </div>
 }
 
